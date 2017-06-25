59a7183c0bae4b636623ea2c47a1955ca4da3416
Martynas Kanapinskas <martis347@gmail.com>
2017-06-25 17:46:34 +0300

    Some Clean-up.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component';
 import {GithubModule} from './containers/github/github.module';
 import {HomeModule} from './containers/home/home.module';
 import {MainModule} from './containers/main/main.module';
 import {ServicesModule} from './services/services.module';
 import {MDL} from "./misc/MaterialDesignLiteUpgradeElement";
-import {CommonModule} from "@angular/common";
 import {LoginGuard} from "./routing/login-guard";
 import {AppRoutingModule} from "./routing/app-routing.module";
 
@@ -19,11 +14,6 @@ import {AppRoutingModule} from "./routing/app-routing.module";
     MDL,
   ],
   imports: [
-    BrowserModule,
-    CommonModule,
-    BrowserAnimationsModule,
-    FormsModule ,
-    HttpModule,
     GithubModule,
     HomeModule,
     MainModule,
